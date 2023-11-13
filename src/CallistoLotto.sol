bfddaf40f50c581e796dc15b1c3bc8bb7492fbd0
0xKits <kitsgit@gmail.com>
2023-11-13 15:50:30 +0530

    added getter for prizepool
    read the title

--- src/CallistoLotto.sol
+++ src/CallistoLotto.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {VRFCoordinatorV2Interface} from "chainlink-brownie-contracts/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFCoordinatorV2Interface} from
+    "chainlink-brownie-contracts/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import {VRFConsumerBaseV2} from "chainlink-brownie-contracts/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -16,11 +17,7 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
     event LotteryEnded(uint256 lottoId, uint256 endTime);
     event LotteryResultDrawn(uint256 lottoId, uint8[(5)] winningNumbers);
     event TicketPurchased(address buyer, uint256 ticketId, uint8[(5)] numbers);
-    event TicketClaimed(
-        address claimer,
-        uint256 ticketId,
-        uint256 prizePoolShare
-    );
+    event TicketClaimed(address claimer, uint256 ticketId, uint256 prizePoolShare);
 
     struct LotteryInstance {
         uint256 startTime;
@@ -59,8 +56,7 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
 
     uint64 s_subscriptionId;
 
-    bytes32 keyHash =
-        0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
+    bytes32 keyHash = 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
 
     uint32 callbackGasLimit = 200000;
 
@@ -91,14 +87,8 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
         CALLISTO = IERC20(callistoToken);
         expiry = _expiry;
         ticketPrice = _ticketPrice;
-        lottoIdToLotto[currentLottoId] = LotteryInstance(
-            block.timestamp,
-            0,
-            0,
-            0,
-            [0, 0, 0, 0, 0],
-            LotteryStatus.IN_PLAY
-        );
+        lottoIdToLotto[currentLottoId] =
+            LotteryInstance(block.timestamp, 0, 0, 0, [0, 0, 0, 0, 0], LotteryStatus.IN_PLAY);
     }
 
     /**
@@ -107,20 +97,11 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
      */
 
     function startNextLotto() public {
-        require(
-            newLottoStartable(),
-            "Lottery: Either lotto is in play or VRF has been requested"
-        );
+        require(newLottoStartable(), "Lottery: Either lotto is in play or VRF has been requested");
 
         currentLottoId++;
-        lottoIdToLotto[currentLottoId] = LotteryInstance(
-            block.timestamp,
-            0,
-            0,
-            0,
-            [0, 0, 0, 0, 0],
-            LotteryStatus.IN_PLAY
-        );
+        lottoIdToLotto[currentLottoId] =
+            LotteryInstance(block.timestamp, 0, 0, 0, [0, 0, 0, 0, 0], LotteryStatus.IN_PLAY);
 
         emit NewLotteryStarted(currentLottoId, block.timestamp);
     }
@@ -128,30 +109,17 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
     function drawLottoResult(uint256 seed) internal {
         lottoIdToLotto[currentLottoId].seed = seed;
         lottoIdToLotto[currentLottoId].status = LotteryStatus.SETTLED;
-        lottoIdToLotto[currentLottoId].winningNumbers = drawWinningNumbers(
-            seed
-        );
+        lottoIdToLotto[currentLottoId].winningNumbers = drawWinningNumbers(seed);
 
-        emit LotteryResultDrawn(
-            currentLottoId,
-            lottoIdToLotto[currentLottoId].winningNumbers
-        );
+        emit LotteryResultDrawn(currentLottoId, lottoIdToLotto[currentLottoId].winningNumbers);
     }
 
     function endLotto() public returns (uint256 requestId) {
-        require(
-            currentLottoEndable(),
-            "Lottery: Either lotto is in play or VRF has been requested"
-        );
+        require(currentLottoEndable(), "Lottery: Either lotto is in play or VRF has been requested");
 
         lottoIdToLotto[currentLottoId].status = LotteryStatus.VRF_REQUESTED;
-        uint256 _requestId = COORDINATOR.requestRandomWords(
-            keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
-        );
+        uint256 _requestId =
+            COORDINATOR.requestRandomWords(keyHash, s_subscriptionId, requestConfirmations, callbackGasLimit, numWords);
         requestIdToLottoId[_requestId] = currentLottoId;
 
         emit LotteryEnded(currentLottoId, block.timestamp);
@@ -164,14 +132,11 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
      *
      */
 
-    function buyTicket(
-        uint8[(5)] memory numbers
-    ) public returns (uint256 ticketId) {
+    function buyTicket(uint8[(5)] memory numbers) public returns (uint256 ticketId) {
         require(isValidNumbers(numbers), "Lottery: Invalid set of numbers!");
 
         require(
-            canBuyTickets(),
-            "Lottery: Either lotto has been settled, VRF has been requested or is awaiting closure"
+            canBuyTickets(), "Lottery: Either lotto has been settled, VRF has been requested or is awaiting closure"
         );
 
         CALLISTO.transferFrom(msg.sender, address(this), ticketPrice);
@@ -179,11 +144,7 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
         currentTicketId++;
         _safeMint(msg.sender, currentTicketId);
 
-        ticketIdToTicket[currentTicketId] = Ticket(
-            currentLottoId,
-            numbers,
-            false
-        );
+        ticketIdToTicket[currentTicketId] = Ticket(currentLottoId, numbers, false);
 
         lottoIdToLotto[currentLottoId].prizePool += ticketPrice;
 
@@ -201,30 +162,19 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
      */
 
     function claimTicket(uint256 ticketId) public {
-        require(
-            ownerOf(ticketId) == msg.sender,
-            "Lottery: You don't own this ticket!"
-        );
+        require(ownerOf(ticketId) == msg.sender, "Lottery: You don't own this ticket!");
 
         require(
-            lottoIdToLotto[ticketIdToTicket[ticketId].lottoId].status ==
-                LotteryStatus.SETTLED,
+            lottoIdToLotto[ticketIdToTicket[ticketId].lottoId].status == LotteryStatus.SETTLED,
             "Lottery: The requested lottery instance is not settled"
         );
 
-        require(
-            !ticketIdToTicket[ticketId].claimed,
-            "Lottery: Nice try, you've already claimed this ticket"
-        );
+        require(!ticketIdToTicket[ticketId].claimed, "Lottery: Nice try, you've already claimed this ticket");
 
         ticketIdToTicket[ticketId].claimed = true;
         uint8[(5)] memory numbers = getTicketNumbers(ticketId);
-        uint256 prizePoolShare = getPrizePoolShare(
-            ticketIdToTicket[ticketId].lottoId,
-            numbers
-        );
-        lottoIdToLotto[ticketIdToTicket[ticketId].lottoId]
-            .prizePool -= prizePoolShare;
+        uint256 prizePoolShare = getPrizePoolShare(ticketIdToTicket[ticketId].lottoId, numbers);
+        lottoIdToLotto[ticketIdToTicket[ticketId].lottoId].prizePool -= prizePoolShare;
         CALLISTO.transfer(msg.sender, prizePoolShare);
 
         emit TicketClaimed(msg.sender, ticketId, prizePoolShare);
@@ -235,17 +185,11 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
      *
      */
 
-    function fulfillRandomWords(
-        uint256,
-        uint256[] memory _randomWords
-    ) internal override {
+    function fulfillRandomWords(uint256, uint256[] memory _randomWords) internal override {
         drawLottoResult(_randomWords[0]);
     }
 
-    function getPrizePoolShare(
-        uint256 lottoId,
-        uint8[(5)] memory numbers
-    ) internal view returns (uint256 share) {
+    function getPrizePoolShare(uint256 lottoId, uint8[(5)] memory numbers) internal view returns (uint256 share) {
         for (uint256 i = 0; i < 5; i++) {
             if (numbers[i] == lottoIdToLotto[lottoId].winningNumbers[i]) {
                 uint256 nc = getNumberCount(lottoId, i, numbers[i]);
@@ -256,19 +200,19 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
     }
 
     function canBuyTickets() internal view returns (bool) {
-        return (block.timestamp <=
-            lottoIdToLotto[currentLottoId].startTime + expiry &&
-            lottoIdToLotto[currentLottoId].status !=
-            LotteryStatus.VRF_REQUESTED &&
-            lottoIdToLotto[currentLottoId].status != LotteryStatus.SETTLED);
+        return (
+            block.timestamp <= lottoIdToLotto[currentLottoId].startTime + expiry
+                && lottoIdToLotto[currentLottoId].status != LotteryStatus.VRF_REQUESTED
+                && lottoIdToLotto[currentLottoId].status != LotteryStatus.SETTLED
+        );
     }
 
     function currentLottoEndable() internal view returns (bool) {
-        return (block.timestamp >=
-            lottoIdToLotto[currentLottoId].startTime + expiry &&
-            lottoIdToLotto[currentLottoId].status != LotteryStatus.SETTLED &&
-            lottoIdToLotto[currentLottoId].status !=
-            LotteryStatus.VRF_REQUESTED);
+        return (
+            block.timestamp >= lottoIdToLotto[currentLottoId].startTime + expiry
+                && lottoIdToLotto[currentLottoId].status != LotteryStatus.SETTLED
+                && lottoIdToLotto[currentLottoId].status != LotteryStatus.VRF_REQUESTED
+        );
     }
 
     function newLottoStartable() internal view returns (bool) {
@@ -279,17 +223,11 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
         lottoIdToPositionToNumberToCounter[currentLottoId][pos][num]++;
     }
 
-    function getNumberCount(
-        uint256 lottoId,
-        uint256 pos,
-        uint256 num
-    ) internal view returns (uint256) {
+    function getNumberCount(uint256 lottoId, uint256 pos, uint256 num) internal view returns (uint256) {
         return (lottoIdToPositionToNumberToCounter[lottoId][pos][num]);
     }
 
-    function drawWinningNumbers(
-        uint256 seed
-    ) internal pure returns (uint8[(5)] memory nums) {
+    function drawWinningNumbers(uint256 seed) internal pure returns (uint8[(5)] memory nums) {
         for (uint256 i = 0; i < 5; i++) {
             seed = uint256(keccak256(abi.encode(seed)));
             nums[i] = uint8(seed % 10);
@@ -297,9 +235,7 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
         return (nums);
     }
 
-    function isValidNumbers(
-        uint8[(5)] memory numbers
-    ) internal pure returns (bool) {
+    function isValidNumbers(uint8[(5)] memory numbers) internal pure returns (bool) {
         for (uint256 i = 0; i < 5; i++) {
             if (numbers[i] > 9) {
                 return (false);
@@ -308,15 +244,11 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
         return (true);
     }
 
-    function getWinningNumbers(
-        uint256 lottoId
-    ) internal view returns (uint8[(5)] memory) {
+    function getWinningNumbers(uint256 lottoId) internal view returns (uint8[(5)] memory) {
         return (lottoIdToLotto[lottoId].winningNumbers);
     }
 
-    function getTicketNumbers(
-        uint256 ticketId
-    ) internal view returns (uint8[(5)] memory) {
+    function getTicketNumbers(uint256 ticketId) internal view returns (uint8[(5)] memory) {
         return (ticketIdToTicket[ticketId].numbers);
     }
 
@@ -325,9 +257,7 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
      *
      */
 
-    function getLottoById(
-        uint256 id
-    )
+    function getLottoById(uint256 id)
         public
         view
         returns (
@@ -349,17 +279,27 @@ contract CallistoLotto is VRFConsumerBaseV2, ERC721 {
         );
     }
 
-    function getTicketById(
-        uint256 id
-    )
-        public
-        view
-        returns (uint256 lottoId, uint8[(5)] memory numbers, bool claimed)
-    {
+    function getTicketById(uint256 id) public view returns (uint256 lottoId, uint8[(5)] memory numbers, bool claimed) {
+        return (ticketIdToTicket[id].lottoId, ticketIdToTicket[id].numbers, ticketIdToTicket[id].claimed);
+    }
+
+    // Returns the number of tokens you would get per positional match. For example :
+    // If winning numbers are [1,2,3,4,5], this function returns the prize return for matching per positon
+    // say, 1 token for matching pos 1, 5 tokens for pos 2 etc.
+    function getLottoPrize(uint256 lottoId) public view returns (uint256[] memory prizeDistribution) {
         return (
-            ticketIdToTicket[id].lottoId,
-            ticketIdToTicket[id].numbers,
-            ticketIdToTicket[id].claimed
+            [
+                (lottoIdToLotto[lottoId].prizePool / 5)
+                    / getNumberCount(lottoId, 0, lottoIdToLotto[lottoId].winningNumbers[0]),
+                (lottoIdToLotto[lottoId].prizePool / 5)
+                    / getNumberCount(lottoId, 1, lottoIdToLotto[lottoId].winningNumbers[1]),
+                (lottoIdToLotto[lottoId].prizePool / 5)
+                    / getNumberCount(lottoId, 2, lottoIdToLotto[lottoId].winningNumbers[2]),
+                (lottoIdToLotto[lottoId].prizePool / 5)
+                    / getNumberCount(lottoId, 3, lottoIdToLotto[lottoId].winningNumbers[3]),
+                (lottoIdToLotto[lottoId].prizePool / 5)
+                    / getNumberCount(lottoId, 4, lottoIdToLotto[lottoId].winningNumbers[4])
+            ]
         );
     }
 }
